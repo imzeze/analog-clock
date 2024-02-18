438f2b7692413ff627660b6fe69d1d0026fb5761
imzeze <jihyekim019@gmail.com>
2024-02-18 19:26:11 +0900

    feat: 시각 수정

--- src/components/HandList.tsx
+++ src/components/HandList.tsx
@@ -4,7 +4,8 @@ import * as S from '@/styles/HandList.style';
 
 export default function HandList() {
   const time = useRecoilValue(nowState);
-  const hour = (time.getHours() % 12) * 30;
+  const hour =
+    (time.getHours() % 12) * 30 + Math.floor(time.getMinutes() / 12) * 6;
   const minute = time.getMinutes() * 6;
   const second = time.getSeconds() * 6;
 
